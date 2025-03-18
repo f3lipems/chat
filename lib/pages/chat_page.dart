fcba24f71874fc16236ba5a2fd45012afea94f6f
Felipe <felipe@visionar.io>
2025-03-18 18:09:00 -0300

    add chat page

--- lib/pages/chat_page.dart
+++ lib/pages/chat_page.dart
@@ -1,3 +1,5 @@
+import 'package:chat/components/messages.dart';
+import 'package:chat/components/new_message.dart';
 import 'package:chat/services/auth/auth_service.dart';
 import 'package:flutter/material.dart';
 
@@ -7,18 +9,29 @@ class ChatPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            Text('Chat Page'),
-            ElevatedButton(
-              onPressed: () => AuthService().logout(),
-              child: Text('Logout'),
-            ),
-          ],
-        ),
+      appBar: AppBar(
+        title: Text('Visionar Chat'),
+        actions: [
+          DropdownButton(
+            icon: Icon(Icons.more_vert, color: Theme.of(context).primaryColor),
+            onChanged: (value) {
+              if (value == 'logout') {
+                AuthService().logout();
+              }
+            },
+            items: [
+              DropdownMenuItem(
+                value: 'logout',
+                child: Row(
+                  spacing: 10,
+                  children: [Icon(Icons.exit_to_app, color: Colors.black), Text('Logout')],
+                ),
+              ),
+            ],
+          ),
+        ],
       ),
+      body: SafeArea(child: Column(children: [Expanded(child: Messages()), NewMessage()])),
     );
   }
 }

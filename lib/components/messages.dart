41403a0a9f750ee25374e0d1bd7cd0680d8420fb
Felipe <felipe@visionar.io>
2025-03-25 17:48:35 -0300

    add mesage component

--- lib/components/messages.dart
+++ lib/components/messages.dart
@@ -1,3 +1,7 @@
+import 'package:chat/components/message_bubble.dart';
+import 'package:chat/models/chat_message.dart';
+import 'package:chat/services/auth/auth_service.dart';
+import 'package:chat/services/chat/chat_service.dart';
 import 'package:flutter/material.dart';
 
 class Messages extends StatelessWidget {
@@ -5,8 +9,31 @@ class Messages extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Center(
-      child: Text('Mensages'),
+    final currentUser = AuthService().currentUser;
+
+    return StreamBuilder<List<ChatMessage>>(
+      stream: ChatService().messagesStream(),
+      builder: (ctx, snapshot) {
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Center(child: CircularProgressIndicator());
+        } else if (!snapshot.hasData || snapshot.data!.isEmpty) {
+          return Center(child: Text('Não há mensagens'));
+        } else {
+          final msgs = snapshot.data;
+          return ListView.builder(
+            reverse: true,
+            itemCount: msgs!.length,
+            itemBuilder: (ctx, index) {
+              final message = msgs[index];
+              return MessageBubble(
+                key: ValueKey(msgs[index].id),
+                message: msgs[index],
+                belongToCurrentUser: msgs[index].userId == currentUser?.id,
+              );
+            },
+          );
+        }
+      },
     );
   }
 }

26707532f01bd8da29ab0b7d712efdd15b1917d8
Felipe <felipe@visionar.io>
2025-03-27 18:44:26 -0300

    redesig message bubble

--- lib/components/message_bubble.dart
+++ lib/components/message_bubble.dart
@@ -1,40 +1,71 @@
+import 'dart:io';
+
 import 'package:chat/models/chat_message.dart';
 import 'package:flutter/material.dart';
 
 class MessageBubble extends StatelessWidget {
+  static const defaultImage = 'assets/images/avatar.png';
   final ChatMessage message;
   final bool belongToCurrentUser;
 
   const MessageBubble({super.key, required this.message, required this.belongToCurrentUser});
 
+  Widget _showAvatar(String imageUrl) {
+    ImageProvider imageProvider;
+    final imageUri = Uri.parse(imageUrl);
+
+    if(imageUrl.contains(defaultImage)) {
+      imageProvider = AssetImage(defaultImage);
+    } else if (imageUri.scheme.contains('http')) {
+      imageProvider = NetworkImage(imageUrl.toString());
+    } else {
+      imageProvider = FileImage(File(imageUrl.toString()));
+    }
+
+    return Positioned(
+      top: 0,
+      left: belongToCurrentUser ? null : 8,
+      right: belongToCurrentUser ? 8 : null,
+      child: CircleAvatar(
+        backgroundColor: Colors.red,
+        backgroundImage: imageProvider,
+      ),
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    return Row(
-      mainAxisAlignment: belongToCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+    return Stack(
       children: [
-        Container(
-          decoration: BoxDecoration(
-            color: belongToCurrentUser ? Colors.grey[300] : Theme.of(context).primaryColorDark,
-            borderRadius: BorderRadius.only(
-              topLeft: Radius.circular(12),
-              topRight: Radius.circular(12),
-              bottomLeft: belongToCurrentUser ? Radius.circular(12) : Radius.circular(0),
-              bottomRight: belongToCurrentUser ? Radius.circular(0) : Radius.circular(12),
-            ),
-          ),
-          width: 300,
-          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
-          margin: EdgeInsets.symmetric(vertical: 4, horizontal: 8),
-          child: Column(
-            children: [
-              Text(
-                message.userName,
-                style: TextStyle(fontWeight: FontWeight.bold, color: belongToCurrentUser ? Colors.black : Colors.white),
+        Row(
+          mainAxisAlignment: belongToCurrentUser ? MainAxisAlignment.end : MainAxisAlignment.start,
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                color: belongToCurrentUser ? Colors.grey[300] : Theme.of(context).primaryColorDark,
+                borderRadius: BorderRadius.only(
+                  topLeft: Radius.circular(12),
+                  topRight: Radius.circular(12),
+                  bottomLeft: belongToCurrentUser ? Radius.circular(12) : Radius.circular(0),
+                  bottomRight: belongToCurrentUser ? Radius.circular(0) : Radius.circular(12),
+                ),
               ),
-              Text(message.text, style: TextStyle(color: belongToCurrentUser ? Colors.black : Colors.white)),
-            ],
-          ),
+              width: 300,
+              padding: EdgeInsets.symmetric(vertical: 10, horizontal: 16),
+              margin: EdgeInsets.symmetric(vertical: 8, horizontal: 8),
+              child: Column(
+                children: [
+                  Text(
+                    message.userName,
+                    style: TextStyle(fontWeight: FontWeight.bold, color: belongToCurrentUser ? Colors.black : Colors.white),
+                  ),
+                  Text(message.text, style: TextStyle(color: belongToCurrentUser ? Colors.black : Colors.white)),
+                ],
+              ),
+            ),
+          ],
         ),
+        _showAvatar(message.userImageUrl),
       ],
     );
   }
